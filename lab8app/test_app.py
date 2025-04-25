9968f665c511261b90fa22c985a811c430d71b22
codentell <codentell@gmail.com>
2025-04-24 22:22:29 -0700

    add more lab9 and lab8

--- lab8app/test_app.py
+++ lab8app/test_app.py
@@ -1,33 +1,7 @@
-# lab8app/app.py
+import requests
 
-from fastapi import FastAPI, Request
-import mlflow
-import mlflow.pyfunc
-import uvicorn
+data = {"data": [[0.0381, 0.0507, 0.0617, 0.0219, -0.0442, -0.0348, -0.0434, -0.0026, 0.0199, -0.0176]]}
 
-app = FastAPI()
-
-# Optional but good practice — set tracking URI
-mlflow.set_tracking_uri("https://mlflow-tracking-174134742093.us-west2.run.app")
-mlflow.set_experiment("lab8-experiment")
-
-# Load model using full artifact URI
-MODEL_URI = "mlflow-artifacts:/6/9becda7e7f2c458989dd6235cf570924/artifacts/metaflow_train"
-
-model = mlflow.pyfunc.load_model(MODEL_URI)
-
-@app.post("/predict")
-async def predict(request: Request):
-    payload = await request.json()
-    input_data = payload.get("data")
-    prediction = model.predict(input_data)
-    return {"prediction": prediction.tolist()}
-
-# Run with:
-# uvicorn test_app:app --reload
-
-
-# curl -X POST http://127.0.0.1:8000/predict \
-#   -H "Content-Type: application/json" \
-#   -d '{"data": [[0.0381, 0.0507, 0.0617, 0.0219, -0.0442, -0.0348, -0.0434, -0.0026, 0.0199, -0.0176]]}'
-
+url = 'http://127.0.0.1:8000/predict'
+response = requests.post(url, json=data)
+print(response.json())
